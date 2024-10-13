9772139aad919115df85eafb5d5af228821c44ea
Haintp <hainghiem@gmail.com>
2024-10-13 07:00:28 +0700

    Commit: [Add] Offload Media [Add] Rank Math Pro

---
+++ wp-content/plugins/seo-by-rank-math-pro/vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'rankmath/seo-by-rank-math-pro',
+        'pretty_version' => 'dev-develop',
+        'version' => 'dev-develop',
+        'reference' => '92636c304c7c957a63393b4e3c715fe0bb41f62e',
+        'type' => 'wordpress-plugin',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'rankmath/seo-by-rank-math-pro' => array(
+            'pretty_version' => 'dev-develop',
+            'version' => 'dev-develop',
+            'reference' => '92636c304c7c957a63393b4e3c715fe0bb41f62e',
+            'type' => 'wordpress-plugin',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
